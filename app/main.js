4d6c9b43f9eb5a6fb881f09f9535e234b87beb71
tesw yew isal <278153+cnmade@users.noreply.github.com>
2021-08-30 15:46:02 +0800

    停用GPU加速逻辑

--- app/main.js
+++ app/main.js
@@ -39,6 +39,9 @@ const createWindow = () => {
 
 };
 
+app.disableHardwareAcceleration();
+app.commandLine.appendSwitch("disable-software-rasterizer");
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
